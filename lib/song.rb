0f2f325da414e6c0039973346e5dda064c64414a
Jenny Chu <jchudesign0930@gmail.com>
2019-04-08 21:01:54 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,7 +2,10 @@ require 'pry'
 
 class Song
   
-  extend memorable
+  extend Memorable::ClassMethods
+  include Memorable::InstanceMethods
+  extend Findable
+  include Paramable
   
   attr_accessor :name
   attr_reader :artist
