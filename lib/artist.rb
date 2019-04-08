8512fb5e4d6561c06e77126a423c778b4831731d
Jenny Chu <jchudesign0930@gmail.com>
2019-04-08 21:35:44 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,14 +1,19 @@
 require 'pry'
 
 class Artist
+  
+  extend Memorable::ClassMethods
+include Memorable::InstanceMethods
+
   attr_accessor :name
   attr_reader :songs
 
   @@artists = []
 
   def initialize
-    @@artists << self
+    self.class.all << self
     @songs = []
+end
   end
 
   def self.find_by_name(name)
@@ -19,13 +24,7 @@ class Artist
     @@artists
   end
 
-  def self.reset_all
-    self.all.clear
-  end
-
-  def self.count
-    self.all.count
-  end
+ 
 
   def add_song(song)
     @songs << song
--- lib/song.rb
+++ lib/song.rb
@@ -1,14 +1,19 @@
 require 'pry'
 
 class Song
+  
+  extend Memorable::ClassMethods
+include Memorable::InstanceMethods
+  
   attr_accessor :name
   attr_reader :artist
+  
 
   @@songs = []
 
   def initialize
-    @@songs << self
-  end
+    self.class.all << self
+end
 
   def self.find_by_name(name)
     @@songs.detect{|a| a.name == name}
@@ -18,13 +23,6 @@ class Song
     @@songs
   end
 
-  def self.reset_all
-    self.all.clear
-  end
-
-  def self.count
-    self.all.count
-  end
 
   def artist=(artist)
     @artist = artist
